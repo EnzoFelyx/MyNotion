4b27d1d99529220566b1e1720e7b2a92ba12d352
EnzoFelyx <enzofelyx@gmail.com>
2025-01-03 18:31:48 -0300

    mydata

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,8 +1,8 @@
 const PAGES = [
   { id: "1", title: "React Native" },
-  { id: "2", title: "Expo" },
-  { id: "3", title: "Links" },
-  { id: "4", title: "Projetos" },
+  { id: "2", title: "Flutter" },
+  { id: "3", title: "Kotlin" },
+  { id: "4", title: "Swift" },
   { id: "5", title: "Node.js" },
   { id: "6", title: "Expo Router" },
   { id: "7", title: "Youtube" },
@@ -27,8 +27,27 @@ const RECENT = [
     cover:
       "https://blog.rocketseat.com.br/content/images/size/w1000/2024/04/react-native-1.png",
   },
-  { id: "2", title: "Expo", cover: undefined },
-  { id: "3", title: "Links", cover: undefined },
+  {
+    id: "2",
+    title: "Flutter",
+    cover:
+      "https://github.com/user-attachments/assets/3e2d3e36-2eca-4e3f-8ac3-fcd55737fa9e",
+  },{
+    id: "3",
+    title: "Kotlin",
+    cover:
+      "https://github.com/user-attachments/assets/b0982f49-1ca3-43d7-a01e-8c60be007a91",
+  },
+  {
+    id: "4",
+    title: "Swift",
+    cover:
+      "https://github.com/user-attachments/assets/a3ea566b-641f-4e5a-a598-4cacfa6efd83",
+  },
+  {
+    id: "5",
+    title: "Node.js",
+  },
 ]
 
 export const DATA = { PAGES, RECENT }
